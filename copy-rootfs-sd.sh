8305e4f71219d52d5a6432f3791b42f30b924929
Konstantin Lampalzer <konstantin@localhost.systems>
2022-01-20 18:41:50 +0100

    Rename

--- copy-rootfs-sd.sh
+++ copy-rootfs-sd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Mount the SSD as /mnt
+# Mount the SD as /mnt
 sudo mount /dev/mmcblk1p1 /mnt
 # Copy over the rootfs from the SD card to the SSD
 sudo rsync -axHAWX --numeric-ids --info=progress2 --exclude={"/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/","/media/*","/lost+found"} / /mnt
-# We want to keep the SSD mounted for further operations
-# So we do not unmount the SSD
+# We want to keep the SD mounted for further operations
+# So we do not unmount the SD
